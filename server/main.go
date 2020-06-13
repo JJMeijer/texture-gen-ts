e5ec422577c59fab761d278c5c7350ab5cc235b6
Johan <6570485+tserest@users.noreply.github.com>
2020-06-13 22:57:50 +0200

    Add router & Basic Logging

--- server/main.go
+++ server/main.go
@@ -3,15 +3,47 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/felixge/httpsnoop"
+	"github.com/gorilla/mux"
 )
 
+func requestLogger(h http.Handler) http.Handler {
+	handlerFunction := func(w http.ResponseWriter, r *http.Request) {
+		h.ServeHTTP(w, r)
+
+		uri := r.URL.String()
+		method := r.Method
+		proto := r.Proto
+
+		m := httpsnoop.CaptureMetrics(h, w, r)
+
+		code := m.Code
+		size := m.Written
+		duration := m.Duration
+
+		log.Println(method, uri, proto, code, size, duration)
+	}
+
+	return http.HandlerFunc(handlerFunction)
+}
+
 func main() {
-	fs := http.FileServer(http.Dir("./dist"))
-	http.Handle("/", fs)
+	router := mux.NewRouter()
+
+	// Static Files (SPA)
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./dist"))).Methods("GET")
+
+	// Setup Server
+	server := &http.Server{
+		Handler:      requestLogger(router),
+		Addr:         ":3000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
 
 	log.Println("Listening on :3000...")
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(server.ListenAndServe())
 }
