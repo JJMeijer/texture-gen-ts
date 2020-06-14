a3b6b65f1f50246c84191e5e75a7fbd1c76faea6
johan <6570485+tserest@users.noreply.github.com>
2020-06-14 22:17:25 +0200

    Try debugging

--- src/components/TextureCanvas.tsx
+++ src/components/TextureCanvas.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './TextureCanvas.css';
 
+console.log('HI from in here');
+
 export const TextureCanvas: React.FC = () => (
   <canvas width="400" height="400" id="texture"></canvas>
 );
